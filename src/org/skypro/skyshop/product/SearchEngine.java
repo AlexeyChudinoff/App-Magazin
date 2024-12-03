b751321a4351d5d2105ce98e513a7005d196b3e4
AlexeyChudinoff <201900@mail.ru>
2024-12-04 02:06:23 +0700

    HW0212_1

--- src/org/skypro/skyshop/product/SearchEngine.java
+++ src/org/skypro/skyshop/product/SearchEngine.java
@@ -1,47 +1,47 @@
 package org.skypro.skyshop.product;
 
 import java.util.Arrays;
-import java.util.Objects;
-import java.util.Scanner;
 
 public class SearchEngine {
 
 
- // public String strSearch;
-  private final String nameProduct;
-  private final String nameArticle;
-  private final String textArticle;
+  /*public String poleSearch;
+  public void poleSearch (String poleSearch) {
+    this.poleSearch = poleSearch;
+  }*/
 
-  public SearchEngine(String nameProduct, String nameArticle, String textArticle) {
-    this.nameProduct = nameProduct;
-    this.nameArticle = nameArticle;
-    this.textArticle = textArticle;
-  }
-  public SearchEngine[] searchable = new SearchEngine[0];
+ public  SearchEngine[] searchable;
 
   public void GenerateSearchable(int size) {
     System.out.println("GenerateSearchable SearchEngine");
-    SearchEngine[] searchable = new SearchEngine[size];
-    System.out.println("создан массив searchable[" + size +"]");
-    for (int i = 0; i < size; i++) {
-      searchable[i] = null;
-      System.out.println(searchable[i]);
-    }
+    searchable = new SearchEngine[size];
+    System.out.println("создан массив searchable[" + size + "]");
   }
 
-  public void add (String nameProduct, String nameArticle, String textArticle) {
-    System.out.println("  add SearchEngine");
-    String answer = "Невозможно добавить продукт";
+  public void add(String nameProduct) {
+    String answer = "Нет места Невозможно добавить продукт " + nameProduct;
     for (int i = 0; i < searchable.length; i++) {
       if (searchable[i] == null) {
-        answer = "Add " + nameProduct + " " + nameArticle + " " +textArticle;
-        searchable[i] = new SearchEngine(nameProduct, nameArticle,textArticle);
+        answer = "add :  " + nameProduct ;
+        searchable[i] = new SearchEngine();
         break;
       }
     }
     System.out.println(answer);
-    System.out.println("-------------");
   }
+
+  public void add(String nameArticle, String textArticle) {
+    String answer = "Нет места Невозможно добавить продукт " + nameArticle;
+    for (int i = 0; i < searchable.length; i++) {
+      if (searchable[i] == null) {
+        answer = "add :  " + nameArticle +  textArticle;
+        searchable[i] = new SearchEngine();
+        break;
+      }
+    }
+    System.out.println(answer);
+  }
+
 
   public void search(String strSearch) {
     System.out.println("'search SearchEngine'");
@@ -55,48 +55,19 @@ public class SearchEngine {
     }
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+  public void printSearchEngine() {
+    System.out.println("printSearchEngine");
+    for (SearchEngine object : searchable) {
+      System.out.println(object);
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    SearchEngine that = (SearchEngine) o;
-    return Objects.deepEquals(searchable, that.searchable) && Objects.equals(
-        nameProduct, that.nameProduct) && Objects.equals(nameArticle, that.nameArticle)
-        && Objects.equals(textArticle, that.textArticle);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(Arrays.hashCode(searchable), nameProduct, nameArticle, textArticle);
   }
 
   @Override
   public String toString() {
     return "SearchEngine{" +
-        "nameArticle='" + nameArticle + '\'' +
-        ", searchable=" + Arrays.toString(searchable) +
-        ", nameProduct='" + nameProduct + '\'' +
-        ", textArticle='" + textArticle + '\'' +
+        "searchable=" + Arrays.toString(searchable) +
         '}';
   }
-/*public SearchEngine[] GenerateSearchable() {
-    System.out.println("GenerateSearchable");
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Введите размер массива"
-        + " поиска (Searchable): ");
-    int size = sc.nextInt();
-    SearchEngine searchable[] = new SearchEngine[size];
-    System.out.println("создан массив searchable[]");
-    for (int i = 0; i < size; i++) {
-      searchable[i] = null;
-      System.out.println(searchable[i]);
-    }
-    return searchable;
-  }*/
 
 
 }//class
--- src/org/skypro/skyshop/product/Searchable.java
+++ src/org/skypro/skyshop/product/Searchable.java
@@ -2,15 +2,14 @@ package org.skypro.skyshop.product;
 
 public interface Searchable {
 
-  String SearchTerm();
-  String getTipContent();
+  String searchTerm();
+  String searchTipContent();
 
-  //String SearchAble();
-
-  default  void getStringRepresentation() {
+   default  void getStringRepresentation() {
     System.out.println("getStringRepresentation");
-    System.out.println("Имя объекта: " + SearchTerm() + "-" +
-        " тип объекта: " + getTipContent());
+
+    System.out.println("Имя объекта: " + searchTerm() + " ;  " +
+        " тип объекта: " + searchTipContent());
   }
 
 
