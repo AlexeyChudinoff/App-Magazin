b2627c36f84183c624044130db59589ac2f33863
AlexeyChudinoff <201900@mail.ru>
2024-12-05 13:45:46 +0700

    HW0212_2

--- src/org/skypro/skyshop/searchProduct/Searchable.java
+++ src/org/skypro/skyshop/searchProduct/Searchable.java
@@ -5,12 +5,15 @@ public interface Searchable {
   String searchTerm();
 
   String searchTipContent();
+  // String searchAble();
 
   // вместо toString
   default void getStringRepresentation() {
     System.out.println("Имя объекта: " + searchTerm() + " ;  " +
         " тип объекта: " + searchTipContent());
   }
+
+
 }
 
 
