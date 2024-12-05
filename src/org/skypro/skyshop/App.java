c19e2bf5866ecd660fcbe72cd9a9759922e49389
AlexeyChudinoff <201900@mail.ru>
2024-12-05 21:58:16 +0700

    HW0212_2

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -39,30 +39,38 @@ public class App {
     Searchable article4 = new Article("article4", "Инструкция к article4");
     Searchable product1 = new FixPriceProduct("product1");
     Searchable product2 = new SimpleProduct("product2", 50);
-     Product product4 = new SimpleProduct("product4", 50);
+    Product product4 = new SimpleProduct("product4", 50);
     Searchable product3 = new DiscountedProduct("product3", 1000, 50);
+    Searchable article5 = new Article("article5", "Инструкция к article5");
+    Searchable article6 = new Article("article6", "Инструкция к article6");
+    Searchable article7 = new Article("article7", "Инструкция к article7");
 
-    searchEngine.GenerateSearchable(7);
+    searchEngine.GenerateSearchable(10);
+    System.out.println();
+    //searchEngine.addBasket(copyBasket);
+    System.out.println();
+    searchEngine.printSearchEngine();
     System.out.println();
     searchEngine.add(article);
     searchEngine.add1(product4);
     searchEngine.add(article1);
     searchEngine.add(article2);
     searchEngine.add(article3);
+    searchEngine.add(article4);
+    searchEngine.add(article5);
+    searchEngine.add(article6);
+    searchEngine.add(article7);
     searchEngine.add(product1);
     searchEngine.add(product2);
     searchEngine.add(product3);
-    System.out.println();
-    searchEngine.printSearchEngine();
     System.out.println();
     searchEngine.printGetStringRepresentation();
     System.out.println();
     searchEngine.search("Инструкция");
     System.out.println();
+
     searchEngine.setCopyBasket();
     System.out.println();
-
-
 
 
   }//maim
