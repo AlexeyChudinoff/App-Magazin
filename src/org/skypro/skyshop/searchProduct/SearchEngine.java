4f05d72ef20240e957a84f8aca90399f55e7fa3f
AlexeyChudinoff <201900@mail.ru>
2024-12-05 21:58:16 +0700

    HW0212_2

--- src/org/skypro/skyshop/searchProduct/SearchEngine.java
+++ src/org/skypro/skyshop/searchProduct/SearchEngine.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import org.skypro.skyshop.product.Basket;
 import org.skypro.skyshop.product.Product;
-import org.skypro.skyshop.product.SimpleProduct;
 
 public class SearchEngine {
 
@@ -40,27 +39,15 @@ public class SearchEngine {
     System.out.println(answer);
   }
 
-
-  /*public void add(Searchable searchTerm) {
-    if (id == searchable.length) {
-      System.out.println("невозможно добавить " + searchTerm);
-      return;
-    }
-    searchable[id] = searchTerm;
-    System.out.println("add :" + searchTerm);
-    id++;
-  }*/
-
-
   public void search(String find) {
     System.out.println("search");
     ArrayList<String> findeProduct = new ArrayList<>();
     for (int i = 0; i < searchable.length; i++) {
       if (searchable[i].searchTerm().contains(find)) {
         findeProduct.add(searchable[i].searchTerm());
-        if (findeProduct.size() == 5) {
+      }
+      if (findeProduct.size() == 5) {
           break;
-        }
       }
     }
     for (int i = 0; i < findeProduct.size(); i++) {
@@ -93,19 +80,32 @@ public class SearchEngine {
     return Arrays.toString(searchable);
   }
 
+  Searchable[] copyBasket = new Product[Basket.getProductBasket().length];
+
   public void setCopyBasket() {
     System.out.println("setCopyBasket");
-    Searchable[] copyBasket = new Product[Basket.getProductBasket().length];
-    {
-      for (int i = 0; i < copyBasket.length; i++) {
-        copyBasket[i] = Basket.getProductBasket()[i];
-        //System.out.println(copyBasket[i]);
-      }
+    for (int i = 0; i < copyBasket.length; i++) {
+      copyBasket[i] = Basket.getProductBasket()[i];
     }
-    System.out.println(Arrays.toString(copyBasket));
+    for (Searchable object : copyBasket) {
+      System.out.println(object);
+    }
   }
 
-
+  //                       copyBasket
+  public void addBasket(Product getNameProduct) {
+    String answer = "Невозможно добавить: " + getNameProduct;
+    for (int i = 0; i < searchable.length; i++) {
+      if (searchable[i] == null) {
+        answer = "add: " + getNameProduct;
+        searchable[i] = getNameProduct;
+      }
+    }
+    System.out.println(answer);
+    for (Searchable object : searchable) {
+      System.out.println(object);
+    }
+  }
 
 
 }//class
