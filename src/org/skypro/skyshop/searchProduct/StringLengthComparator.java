81abac30c4c1c4b311442248815523e438729c4f
Алексей <201900@mail.ru>
2025-01-13 20:35:20 +0700

    HW_Set_3

--- src/org/skypro/skyshop/searchProduct/StringLengthComparator.java
+++ src/org/skypro/skyshop/searchProduct/StringLengthComparator.java
@@ -1,6 +1,24 @@
 package org.skypro.skyshop.searchProduct;
 
 import java.util.Comparator;
+//это для сортировки по TreeMap
+//import java.util.Set;
+//import java.util.TreeSet;
+//
+//public class Main {
+//  public static void main(String[] args) {
+//    Set<String> set = new TreeSet<>(new StringLengthComparator());
+//
+//    set.add("Apple");
+//    set.add("Banana");
+//    set.add("Fig");
+//    set.add("Cherry");
+//
+//    for (String fruit : set) {
+//      System.out.println(fruit);
+//    }
+//  }
+//}
 
 public class StringLengthComparator implements Comparator<String> {
 
